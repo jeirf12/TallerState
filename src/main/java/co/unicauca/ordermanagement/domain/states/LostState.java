3c5da80573bf9aaa3441273946b5c00004eb3ac5
Jhonfer Ruiz <jhonfer.ruiz@gmail.com>
2020-11-08 21:32:15 -0500

    primer commit

---
+++ src/main/java/co/unicauca/ordermanagement/domain/states/LostState.java
@@ -0,0 +1,43 @@
+package co.unicauca.ordermanagement.domain.states;
+
+import co.unicauca.ordermanagement.domain.Order;
+
+/**
+ * Estado perdido
+ *
+ * @author Julio Hurtado, Libardo Pantoja
+ */
+public class LostState extends OrderState {
+
+    /**
+     * @param order to be processed
+     */
+    public LostState(Order order) {
+        super(order);
+    }
+
+    /**
+     * @return description
+     */
+    @Override
+    public String getStateDescription() {
+        return "Perdida";
+    }
+
+    /**
+     * @return nuevo estado como CanceledState
+     */
+    @Override
+    public OrderState cancel() {
+        return new CanceledState(getOrder());
+    }
+
+    /**
+     * @param number
+     * @return nuevo estado como DeliveredState
+     */
+    @Override
+    public OrderState orderSendOut() {
+        return new SendState(getOrder());
+    }
+}
---
+++ src/main/java/co/unicauca/ordermanagement/domain/states/PayedState.java
@@ -0,0 +1,33 @@
+package co.unicauca.ordermanagement.domain.states;
+
+import co.unicauca.ordermanagement.domain.Order;
+
+/**
+ *  estado pagado
+ * @author Jhonny Rosero
+ * @author Jhonfer Ruiz
+ */
+public class PayedState extends OrderState{
+
+    public PayedState(Order order) {
+        super(order);
+    }
+    /**
+     * @return description
+     */
+    @Override
+    public String getStateDescription() {
+        return "Pagada";
+    }
+    
+    /**
+     * @return nuevo estado como DeliveredState
+     */
+    @Override
+    public OrderState orderSendOut() {
+        if(!getOrder().isPaymentReceived()){
+            throw new IllegalStateException("An order should not be send out when payment is not received.");
+        }
+        return new SendState(getOrder());
+    }
+}
